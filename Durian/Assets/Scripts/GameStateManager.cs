85eee71fdadaddcb3922856b41dc62c55e198105
Richard But <butr@uci.edu>
2018-05-17 00:44:37 -0700

    Basic Setup
    Added basic board w/ tiles
    Added UI w/ no code
    Added basic turn system (unimplemented)

---
+++ Durian/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour {
+    public GameObject rollDiceUI;
+
+    [HideInInspector]
+    public GameStates currentState;
+
+    public enum GameStates
+    {
+        PregameSetting,
+        Player1Turn,
+        Player1Move,
+        Player2Turn,
+        Player2Move,
+        Combat,
+        GameOver
+    }
+
+	// Use this for initialization
+	void Start () {
+        currentState = GameStates.PregameSetting;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (currentState)
+        {
+            case (GameStates.PregameSetting):
+                currentState = GameStates.Player1Turn;
+                break;
+            case (GameStates.Player1Turn):
+                rollDiceUI.SetActive(true);
+                break;
+            case (GameStates.Player1Move):
+                rollDiceUI.SetActive(false);
+                currentState = GameStates.Player2Turn;
+                break;
+            case (GameStates.Player2Turn):
+                rollDiceUI.SetActive(true);
+                break;
+            case (GameStates.Player2Move):
+                rollDiceUI.SetActive(false);
+                currentState = GameStates.Player1Turn;
+                break;
+            case (GameStates.Combat):
+                break;
+            case (GameStates.GameOver):
+                break;
+        }
+	}
+}
