d88e8dec9fd75fe97ed55caa28c75e880ae08330
Rikerd <cursedmarkedsasuke@gmail.com>
2018-05-17 16:18:25 -0700

    Game Logic
    Implemented simple board movement and turn base
    Implemented dice roll

--- Durian/Assets/Scripts/GameStateManager.cs
+++ Durian/Assets/Scripts/GameStateManager.cs
@@ -1,12 +1,24 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameStateManager : MonoBehaviour {
-    public GameObject rollDiceUI;
+    public GameObject[] players;
+    public GameObject[] playersCurrentTile;
 
+    // UI VARIABLES
+    public Text numberRolledText;
+    public Text currentTurnText;
+
+    public Button rollDiceButton;
+    
+    // Game State
     [HideInInspector]
     public GameStates currentState;
+
+    private int numberedRolled;
+    private bool coroutineStarted;
 
     public enum GameStates
     {
@@ -15,6 +27,10 @@ public class GameStateManager : MonoBehaviour {
         Player1Move,
         Player2Turn,
         Player2Move,
+        Player3Turn,
+        Player3Move,
+        Player4Turn,
+        Player4Move,
         Combat,
         GameOver
     }
@@ -22,6 +38,10 @@ public class GameStateManager : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
         currentState = GameStates.PregameSetting;
+
+        numberRolledText.text = "Rolled: -";
+
+        coroutineStarted = false;
 	}
 	
 	// Update is called once per frame
@@ -32,18 +52,60 @@ public class GameStateManager : MonoBehaviour {
                 currentState = GameStates.Player1Turn;
                 break;
             case (GameStates.Player1Turn):
-                rollDiceUI.SetActive(true);
+                currentTurnText.text = "Player 1 Turn";
+                rollDiceButton.enabled = true;
                 break;
             case (GameStates.Player1Move):
-                rollDiceUI.SetActive(false);
-                currentState = GameStates.Player2Turn;
+                numberRolledText.text = "Rolled: " + numberedRolled;
+                
+                if (!coroutineStarted)
+                {
+                    rollDiceButton.enabled = false;
+                    StartCoroutine(movePlayer(0));
+                }
+
                 break;
             case (GameStates.Player2Turn):
-                rollDiceUI.SetActive(true);
+                currentTurnText.text = "Player 2 Turn";
+                rollDiceButton.enabled = true;
                 break;
             case (GameStates.Player2Move):
-                rollDiceUI.SetActive(false);
-                currentState = GameStates.Player1Turn;
+                numberRolledText.text = "Rolled: " + numberedRolled;
+
+                if (!coroutineStarted)
+                {
+                    rollDiceButton.enabled = false;
+                    StartCoroutine(movePlayer(1));
+                }
+
+                break;
+            case (GameStates.Player3Turn):
+                currentTurnText.text = "Player 3 Turn";
+                rollDiceButton.enabled = true;
+                break;
+            case (GameStates.Player3Move):
+                numberRolledText.text = "Rolled: " + numberedRolled;
+
+                if (!coroutineStarted)
+                {
+                    rollDiceButton.enabled = false;
+                    StartCoroutine(movePlayer(2));
+                }
+
+                break;
+            case (GameStates.Player4Turn):
+                currentTurnText.text = "Player 2 Turn";
+                rollDiceButton.enabled = true;
+                break;
+            case (GameStates.Player4Move):
+                numberRolledText.text = "Rolled: " + numberedRolled;
+
+                if (!coroutineStarted)
+                {
+                    rollDiceButton.enabled = false;
+                    StartCoroutine(movePlayer(3));
+                }
+
                 break;
             case (GameStates.Combat):
                 break;
@@ -51,4 +113,59 @@ public class GameStateManager : MonoBehaviour {
                 break;
         }
 	}
+
+    public void RollDice()
+    {
+        numberedRolled = Random.Range(1, 6);
+
+        if (currentState == GameStates.Player1Turn)
+        {
+            currentState = GameStates.Player1Move;
+        }
+        else if (currentState == GameStates.Player2Turn)
+        {
+            currentState = GameStates.Player2Move;
+        }
+        else if (currentState == GameStates.Player3Turn)
+        {
+            currentState = GameStates.Player3Move;
+        }
+        else if (currentState == GameStates.Player4Turn)
+        {
+            currentState = GameStates.Player4Move;
+        }
+    }
+
+    IEnumerator movePlayer(int playerIndex)
+    {
+        coroutineStarted = true;
+
+        for (int i = 0; i < numberedRolled; i++)
+        {
+            playersCurrentTile[playerIndex] = playersCurrentTile[playerIndex].GetComponent<BoardTile>().NextBoardTiles[0];
+            players[playerIndex].transform.position = playersCurrentTile[playerIndex].transform.position;
+
+            yield return new WaitForSeconds(0.2f);
+        }
+
+        coroutineStarted = false;
+
+
+        if (currentState == GameStates.Player1Move)
+        {
+            currentState = GameStates.Player2Turn;
+        }
+        else if (currentState == GameStates.Player2Move)
+        {
+            currentState = GameStates.Player3Turn;
+        }
+        else if (currentState == GameStates.Player3Move)
+        {
+            currentState = GameStates.Player4Turn;
+        }
+        else if (currentState == GameStates.Player4Move)
+        {
+            currentState = GameStates.Player1Turn;
+        }
+    }
 }
